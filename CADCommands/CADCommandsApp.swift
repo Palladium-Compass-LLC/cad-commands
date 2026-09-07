6adcf36df229a0f2ecfb7c695f6c37e57d222d79
Palladium Compass LLC <Dev@palladiumcompass.com>
2026-09-07 14:44:30 -0500

    Initial Commit

---
+++ CADCommands/CADCommandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CADCommandsApp.swift
+//  CADCommands
+//
+//  Created by Jonathan T. on 9/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CADCommandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
